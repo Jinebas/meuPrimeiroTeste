bf7b1b840cf80fcaa8495cd3c0b0a3c8b12667b0
Daniel Jin Yamamoto <jinebas@hotmail.com>
2021-09-22 18:54:55 -0300

    Exception handling

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -30,10 +30,34 @@ namespace MyClassesTest
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void FileNameNullOrEmpty_ThrowsArgumentNullException()
         {
             //TODO
-            Assert.Inconclusive();
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingTryCatch()
+        {
+            //TODO
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentException)
+            {
+                //The test was a Success.
+                return;
+            }
+
+            Assert.Fail("Fail expected");
+
+            fp.FileExists("");
         }
     }
 }
