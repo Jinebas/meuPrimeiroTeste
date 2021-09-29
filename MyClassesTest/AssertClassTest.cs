970e02cd314032a8f2263b9d4394fd6da283d00a
Daniel Jin Yamamoto <jinebas@hotmail.com>
2021-09-28 21:30:43 -0300

    Class Assert - AreSame and AreNotSame

--- MyClassesTest/AssertClassTest.cs
+++ MyClassesTest/AssertClassTest.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
 
 namespace MyClassesTest
 {
     [TestClass]
     public class AssertClassTest
     {
+        #region AreEqual/AreNotEqual Tests
         [TestMethod]
         [Owner("ViniciusA")]
         public void AreEqualTest()
@@ -36,6 +38,30 @@ namespace MyClassesTest
 
             Assert.AreNotEqual(str1, str2);
         }
+        #endregion
+
+        #region AreSame/AreNotSame Tests
+        [TestMethod]
+        public void AreSameTest()
+        {
+            FileProcess x = new FileProcess();
+            FileProcess y = x;
+
+            Assert.AreSame(x, y);
+        }
+
+        [TestMethod]
+        public void AreNotSameTest()
+        {
+            FileProcess x = new FileProcess();
+            FileProcess y = new FileProcess();
+
+            Assert.AreNotSame(x, y);
+        }
+
+
+        #endregion
 
     }
+
 }
