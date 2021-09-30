28cdda3c8ba1ee03e2f9f1454898473ea1d399fd
Daniel Jin Yamamoto <jinebas@hotmail.com>
2021-09-30 18:45:07 -0300

    Class Collections

--- MyClasses/PersonClasses/PersonManager.cs
+++ MyClasses/PersonClasses/PersonManager.cs
@@ -25,5 +25,27 @@ namespace MyClasses.PersonClasses
 
             return ret;
         }
+
+        public List<Person> GetPeople()
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person() { FirstName = "Vinicius", LastName = "Andrade" });
+            people.Add(new Person() { FirstName = "Laura", LastName = "Antonia" });
+            people.Add(new Person() { FirstName = "Thiago", LastName = "Paulo" });
+
+            return people;
+        }
+
+        public List<Person> GetSuperVisor()
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(CreatePerson("Vinicius", "Andrade", true));
+            people.Add(CreatePerson("Laura", "Antonia", true));
+
+            return people;
+
+        }
     }
 }
---
+++ MyClassesTest/CollectionAssertClassTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses.PersonClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class CollectionAssertClassTest
+    {
+        [TestMethod]
+        [Owner("ViniciusA")]
+        public void AreCollectionEqualFailsBecauseNoComparerTest()
+        {
+            PersonManager PerMgr = new PersonManager();
+            List<Person> peopleExpected = new List<Person>();
+            List<Person> peopleActual = new List<Person>();
+
+            peopleExpected.Add(new Person() { FirstName = "Vinicius", LastName = "Andrade" });
+            peopleExpected.Add(new Person() { FirstName = "Laura", LastName = "Antonia" });
+            peopleExpected.Add(new Person() { FirstName = "Thiago", LastName = "Paulo" });
+
+            //You shall not pass!!
+            peopleActual = PerMgr.GetPeople();
+
+            CollectionAssert.AreEqual(peopleExpected, peopleActual);
+
+        }
+
+        [TestMethod]
+        [Owner("ViniciusA")]
+        public void AreCollectionEqualWithComparerTest()
+        {
+            PersonManager PerMgr = new PersonManager();
+            List<Person> peopleExpected = new List<Person>();
+            List<Person> peopleActual = new List<Person>();
+
+            peopleExpected.Add(new Person() { FirstName = "Vinicius", LastName = "Andrade" });
+            peopleExpected.Add(new Person() { FirstName = "Laura", LastName = "Antonia" });
+            peopleExpected.Add(new Person() { FirstName = "Thiago", LastName = "Paulo" });
+
+            //You shall not pass!!
+            peopleActual = PerMgr.GetPeople();
+
+            CollectionAssert.AreEqual(peopleExpected, peopleActual,
+                Comparer<Person>.Create((x, y) =>
+                x.FirstName == y.FirstName && x.LastName == y.LastName ? 0: 1));
+
+        }
+
+        [TestMethod]
+        [Owner("ViniciusA")]
+        public void AreCollectionEquivalentTest()
+        {
+            PersonManager PerMgr = new PersonManager();
+            List<Person> peopleExpected = new List<Person>();
+            List<Person> peopleActual = new List<Person>();
+
+            peopleActual = PerMgr.GetPeople();
+
+            peopleExpected.Add(peopleActual[1]);
+            peopleExpected.Add(peopleActual[2]);
+            peopleExpected.Add(peopleActual[0]);
+
+            CollectionAssert.AreEquivalent(peopleExpected, peopleActual);
+
+        }
+
+        [TestMethod]
+        [Owner("ViniciusA")]
+        public void IsCollectionOfTypeTest()
+        {
+            PersonManager PerMgr = new PersonManager();
+            List<Person> peopleActual = new List<Person>();
+
+            peopleActual = PerMgr.GetSuperVisor();
+
+            CollectionAssert.AllItemsAreInstancesOfType(peopleActual, typeof(Supervisor));
+
+        }
+    }
+}
