c67c235d25f6cb200c2453932b2b7f466996c180
Daniel Jin Yamamoto <jinebas@hotmail.com>
2021-09-30 17:34:35 -0300

    Class StringAssert

---
+++ MyClassesTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class StringAssertClassTest
+    {
+        [TestMethod]
+        [Owner("ViniciusA")]
+        public void ContainsTest()
+        {
+            string str1 = "Vinicius Andrade";
+            string str2 = "Andrade";
+
+            StringAssert.Contains(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("ViniciusA")]
+        public void StartsWithTest()
+        {
+            string str1 = "Todos Caixa Alta";
+            string str2 = "Todos Caixa";
+
+            StringAssert.StartsWith(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("ViniciusA")]
+        public void IsAllLoweCaseTest()
+        {
+            Regex reg = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.Matches("todos caixa", reg);
+        }
+
+        [TestMethod]
+        [Owner("ViniciusA")]
+        public void IsNotAllLoweCaseTest()
+        {
+            Regex reg = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.DoesNotMatch("Todos Caixa", reg);
+        }
+    }
+}
