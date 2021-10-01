ee93078eedcea0e361f867ffb0a07d7932dd9fa4
Daniel Jin Yamamoto <jinebas@hotmail.com>
2021-10-01 19:11:54 -0300

    Code Coverage - PersonManager

---
+++ MyClassesTest/PersonManagerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses.PersonClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class PersonManagerTest
+    {
+        [TestMethod]
+        public void CreatePerson_OfTypeEmployeeTest()
+        {
+            PersonManager PerMgr = new PersonManager();
+            Person per;
+
+            per = PerMgr.CreatePerson("Vinicius", "Andrade", false);
+
+            Assert.IsInstanceOfType(per, typeof(Employee));
+             
+        }
+    }
+}
